a07b5c8c51886216471661f8b8e5a64919561c62
Arejunior12 <arejunior543@gmail.com>
2025-07-11 18:24:20 +0100

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+result1 = number1 +number2
+
+result2 = number1 - number2
+
+result3 = number1 * number2
+
+print(f'Sum of {number2} and {number2} is {result1}')
+
+print(f'Difference of {number2} and {number2} is {result2}')
+
+print(f'Product of {number2} and {number2} is {result3}')
